670c055d8511668d019260b215952f1a38b98496
Vigneshwar B <Vigneshwar.2525@xcelevate.org>
2025-08-01 19:57:56 +0530

    Add files via upload

---
+++ FCP001.java
@@ -0,0 +1,10 @@
+public class FCP001{
+public static void main(String[]args){
+int Date = 18;
+int Month = 9;
+int Year = 2003;
+System.out.print( Date + "-" );
+System.out.print( Month + "-" );
+System.out.print( Year );
+}
+}
